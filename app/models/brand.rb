4a3b38aa275c22444f617b3b37fc2ecb4a109896
Thanh NT <thanh.nt@raksul.com>
2021-07-12 14:29:10 +0700

    Add validation rules to models and cascading delete dependencies

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,5 @@
 class Brand < ApplicationRecord
-  has_many :models
+  has_many :models, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,4 +1,7 @@
 class Model < ApplicationRecord
   belongs_to :brand
-  has_many :inventories
+  has_many :inventories, dependent: :destroy
+
+  validates_associated :brand
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
