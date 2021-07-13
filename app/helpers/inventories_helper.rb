332bbd81266dcfe3d6384ecbb4b0f4e33fa999ca
Thanh NT <thanh.nt@raksul.com>
2021-07-13 12:50:58 +0700

    Change select-option helper functions: accept selected parameter

--- app/helpers/inventories_helper.rb
+++ app/helpers/inventories_helper.rb
@@ -3,14 +3,22 @@ module InventoriesHelper
     number_to_currency(price, unit: "VND", format: "%n %u", precision: 0)
   end
 
-  def manufactoring_year_range
-    1990..Time.zone.now.year
+  def manufactoring_year_range(selected=nil)
+    options_for_select(1990..Time.zone.now.year, selected=selected)
   end
 
-  def inventory_status_options(inventory)
+  def inventory_status_options(selected=nil)
     options_for_select(
       Inventory.statuses.map { |k, v|
         [k.titleize, Inventory.statuses.key(v)]
-      }, selected=inventory.status)
+      }, selected=selected)
+  end
+
+  def memory_size_options
+    ["< 16 GB", "16 -> 64 GB", "64 -> 256 GB", "> 256 GB"]
+  end
+
+  def price_options
+    ["< 10 tr", "10 -> 15 tr", "15 -> 20 tr", "20 -> 30 tr", "> 30 tr"]
   end
 end
