c4f57344790d2213e12a0ac73847e11675378e78
Thanh NT <thanh.nt@raksul.com>
2021-07-15 15:27:35 +0700

    Change Devise registrations controller: require role and display name to sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :permit_extra_devise_registration_parameters, if: :devise_controller?
+
+  protected
+
+  def permit_extra_devise_registration_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role_id, :name])
+  end
 end
