b5bfcff9e3e97af68ae60996623fada5a5de147e
Thanh NT <thanh.nt@raksul.com>
2021-07-15 15:26:48 +0700

    Migrate new DB schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2021_07_15_054115) do
   create_table "accounts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", default: "", null: false
     t.bigint "role_id", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
