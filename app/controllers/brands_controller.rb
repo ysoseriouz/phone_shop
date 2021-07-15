d6aaba37dff73f6ace3a27f5b511870ed842722b
Thanh NT <thanh.nt@raksul.com>
2021-07-15 20:45:24 +0700

    Manage user actions using devise and account role

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,12 +1,15 @@
 class BrandsController < ApplicationController
+  before_action :authenticate_account!
+  before_action :authorize_account?, except: [:index]
+
   def index
-    @brands = Brand.all
+    @brands = Brand.order(:name)
   end
 
   def create
     @brand = Brand.new(name: params[:name])
     if @brand.save
-      redirect_to brands_path, notice: "New brand created successfully"
+      redirect_to brands_path, notice: "New brand created successfully."
     else
       redirect_to brands_path, alert: @brand.errors.full_messages
     end
@@ -15,7 +18,7 @@ class BrandsController < ApplicationController
   def update
     @brand = Brand.find(params[:id])
     if @brand.update(name: params[:new_name])
-      redirect_to brands_path, notice: "Brand name updated successfully"
+      redirect_to brands_path, notice: "Brand name updated successfully."
     else
       redirect_to brands_path, alert: @brand.errors.full_messages
     end
@@ -24,9 +27,17 @@ class BrandsController < ApplicationController
   def destroy
     @brand = Brand.find(params[:id])
     if @brand.destroy
-      redirect_to brands_path, notice: "Brand deleted successfully"
+      redirect_to brands_path, notice: "Brand deleted successfully."
     else
       redirect_to brands_path, alert: @brand.errors.full_messages
+    end
+  end
+
+  private
+
+  def authorize_account?
+    unless current_account.manager?
+      redirect_to brands_path, alert: "You are not authorized."
     end
   end
 end
--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -1,8 +1,11 @@
 class ModelsController < ApplicationController
+  before_action :authenticate_account!
+  before_action :authorize_account?
+
   def create
     @model = Model.new(name: params[:name], brand_id: params[:brand_id])
     if @model.save
-      redirect_to brands_path, notice: "New model created successfully"
+      redirect_to brands_path, notice: "New model created successfully."
     else
       redirect_to brands_path, alert: @model.errors.full_messages
     end
@@ -11,7 +14,7 @@ class ModelsController < ApplicationController
   def update
     @model = Model.find(params[:id])
     if @model.update(name: params[:new_name])
-      redirect_to brands_path, notice: "Model name updated successfully"
+      redirect_to brands_path, notice: "Model name updated successfully."
     else
       redirect_to brands_path, alert: @model.errors.full_messages
     end
@@ -20,9 +23,17 @@ class ModelsController < ApplicationController
   def destroy
     @model = Model.find(params[:id])
     if @model.destroy
-      redirect_to brands_path, notice: "Model deleted successfully"
+      redirect_to brands_path, notice: "Model deleted successfully."
     else
       redirect_to brands_path, alert: @model.errors.full_messages
+    end
+  end
+
+  private
+
+  def authorize_account?
+    unless current_account.manager?
+      redirect_to brands_path, alert: "You are not authorized."
     end
   end
 end
