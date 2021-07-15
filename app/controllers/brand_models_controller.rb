ce35f29ebf92256cc431c0c0a8b542134bcffc11
Thanh NT <thanh.nt@raksul.com>
2021-07-15 08:54:51 +0700

    Add alert message to warn if invalid create/update brand or model

--- app/controllers/brand_models_controller.rb
+++ app/controllers/brand_models_controller.rb
@@ -6,40 +6,54 @@ class BrandModelsController < ApplicationController
   def create_brand
     @brand = Brand.new(name: params[:name])
     if @brand.save
-      redirect_to brand_models_index_path
+      redirect_to brand_models_index_path, notice: "New brand created successfully"
+    else
+      redirect_to brand_models_index_path, notice: @brand.errors.full_messages
     end
   end
 
   def create_model
     @model = Model.new(name: params[:name], brand_id: params[:brand_id])
     if @model.save
-      redirect_to brand_models_index_path
+      redirect_to brand_models_index_path, notice: "New model created successfully"
+    else
+      redirect_to brand_models_index_path, notice: @model.errors.full_messages
     end
   end
 
   def update_brand
     @brand = Brand.find(params[:id])
     if @brand.update(name: params[:new_name])
-      redirect_to brand_models_index_path
+      redirect_to brand_models_index_path, notice: "Brand name updated successfully"
+    else
+      redirect_to brand_models_index_path, notice: @brand.errors.full_messages
     end
   end
 
   def update_model
     @model = Model.find(params[:id])
     if @model.update(name: params[:new_name])
-      redirect_to brand_models_index_path
+      redirect_to brand_models_index_path, notice: "Model name updated successfully"
+    else
+      redirect_to brand_models_index_path, notice: @model.errors.full_messages
     end
   end
 
   def destroy_brand
     @brand = Brand.find(params[:id])
-    @brand.destroy
-    redirect_to brand_models_index_path
+    if @brand.destroy
+      redirect_to brand_models_index_path, notice: "Brand deleted successfully"
+    else
+      redirect_to brand_models_index_path, notice: @brand.errors.full_messages
+    end
   end
 
   def destroy_model
     @model = Model.find(params[:id])
-    @model.destroy
-    redirect_to brand_models_index_path
+    if @model.destroy
+      redirect_to brand_models_index_path, notice: "Model deleted successfully"
+    else
+      redirect_to brand_models_index_path, notice: @model.errors.full_messages
+    end
   end
 end
