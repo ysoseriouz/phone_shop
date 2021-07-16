43fb80a0495bbeba96662bc97688b9f753372211
Thanh NT <thanh.nt@raksul.com>
2021-07-16 10:33:03 +0700

    Temporarily comment out select2: wait for UI fixing

--- app/javascript/packs/brand_model_select2.js
+++ app/javascript/packs/brand_model_select2.js
@@ -2,6 +2,6 @@ import $ from 'jquery'
 import 'select2'
 import 'select2/dist/css/select2.css'
 
-window.addEventListener("DOMContentLoaded", () => {
-  $(".brand_model_groups_selection").select2()
-})
+// window.addEventListener("DOMContentLoaded", () => {
+//   $(".brand_model_groups_selection").select2();
+// })
