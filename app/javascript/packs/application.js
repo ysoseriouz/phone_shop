83b44a4434c7718eae5c7a89d40203a3dd0cc07b
Thanh NT <thanh.nt@raksul.com>
2021-07-18 10:56:21 +0700

    Change the way import jQuery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,16 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "jquery"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// Added
+import "select2"
+import "select2/dist/css/select2"
+
+var jQuery = require("jquery")
+
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
