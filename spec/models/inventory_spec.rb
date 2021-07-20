ef0b52d37e49650ae54174150f65c374909a7900
Thanh NT <thanh.nt@raksul.com>
2021-07-20 21:08:37 +0700

    RSpec test: searching using scope

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -3,9 +3,38 @@
 require 'rails_helper'
 
 RSpec.describe Inventory, type: :model do
+  def seed_data       # rubocop:disable Metrics/MethodLength
+    brand1 = Brand.create(name: 'Apple')
+    brand2 = Brand.create(name: 'Samsung')
+    model1 = Model.create(name: 'iPhone X', brand: brand1)
+    model2 = Model.create(name: 'iPhone 7', brand: brand1)
+    model3 = Model.create(name: 'Galaxy Note 10', brand: brand2)
+
+    Inventory.create(
+      model: model1, memory_size: 10, manufactoring_year: 2020,
+      os_version: 'iOS 14', color: 'black', price: 9_000_000, status: :active
+    )
+    Inventory.create(
+      model: model1, memory_size: 32, manufactoring_year: 2020,
+      os_version: 'iOS 14', color: 'green', price: 10_000_000, status: :active
+    )
+    Inventory.create(
+      model: model2, memory_size: 200, manufactoring_year: 2014,
+      os_version: 'Android 5', color: 'black', price: 15_000_000, status: :inactive
+    )
+    Inventory.create(
+      model: model3, memory_size: 300, manufactoring_year: 2021,
+      os_version: 'Android 5', color: 'Green', price: 20_000_000, status: :inactive
+    )
+    Inventory.create(
+      model: model3, memory_size: 256, manufactoring_year: 2019,
+      os_version: 'Android 11', color: 'BLACK', price: 30_000_000, status: :sold
+    )
+  end
+
   describe '.new' do
-    let(:brand) { Brand.create(name: 'Apple') }
-    let(:model) { Model.create(name: 'iPhone X', brand: brand) }
+    let(:brand) { Brand.create(name: 'Apple 2') }
+    let(:model) { Model.create(name: 'iPhone 100', brand: brand) }
     let(:inventory) do
       Inventory.new(
         model: model, memory_size: 256, manufactoring_year: 2020,
@@ -66,6 +95,98 @@ RSpec.describe Inventory, type: :model do
     end
   end
 
-  describe '.update' do
+  describe '.scope' do
+    before(:all) do
+      seed_data
+    end
+
+    after(:all) do
+      Brand.destroy_all
+      Model.destroy_all
+    end
+
+    context 'records found' do
+      it 'by brand' do
+        brand = Brand.find_by(name: 'Apple')
+        inventories = Inventory.by_brand_id(brand.id)
+        expect(inventories.count).to eq(3)
+      end
+      it 'by model' do
+        model = Model.find_by(name: 'iPhone X')
+        inventories = Inventory.by_model_id(model.id)
+        expect(inventories.count).to eq(2)
+      end
+      it 'by manufactoring_year lower bound' do
+        inventories = Inventory.by_manufactoring_year_lower(2020)
+        expect(inventories.count).to eq(3)
+      end
+      it 'by manufactoring_year upper bound' do
+        inventories = Inventory.by_manufactoring_year_upper(2019)
+        expect(inventories.count).to eq(2)
+      end
+      it 'by memory_size range' do
+        inventories = Inventory.by_memory_size('Under 16GB')
+        expect(inventories.count).to eq(1)
+        inventories = Inventory.by_memory_size('From 16GB to 64GB')
+        expect(inventories.count).to eq(1)
+        inventories = Inventory.by_memory_size('From 64GB to 256GB')
+        expect(inventories.count).to eq(2)
+        inventories = Inventory.by_memory_size('Over 256GB')
+        expect(inventories.count).to eq(2)
+      end
+      it 'by price range' do
+        inventories = Inventory.by_price('Under 10 million VND')
+        expect(inventories.count).to eq(2)
+        inventories = Inventory.by_price('From 10 to 15 million VND')
+        expect(inventories.count).to eq(2)
+        inventories = Inventory.by_price('From 15 to 20 million VND')
+        expect(inventories.count).to eq(2)
+        inventories = Inventory.by_price('From 20 to 30 million VND')
+        expect(inventories.count).to eq(2)
+        inventories = Inventory.by_price('Over 30 million VND')
+        expect(inventories.count).to eq(1)
+      end
+      it 'by status' do
+        inventories = Inventory.by_status(:active)
+        expect(inventories.count).to eq(2)
+        inventories = Inventory.by_status(:inactive)
+        expect(inventories.count).to eq(2)
+        inventories = Inventory.by_status(:sold)
+        expect(inventories.count).to eq(1)
+      end
+      it 'by os_version' do
+        inventories = Inventory.by_os_version('iOS 14')
+        expect(inventories.count).to eq(2)
+        inventories = Inventory.by_os_version('Android 5')
+        expect(inventories.count).to eq(2)
+        inventories = Inventory.by_os_version('Android 11')
+        expect(inventories.count).to eq(1)
+      end
+      it 'by color' do
+        inventories = Inventory.by_color('black')
+        expect(inventories.count).to eq(3)
+        inventories = Inventory.by_color('green')
+        expect(inventories.count).to eq(2)
+      end
+    end
+
+    context 'records not found' do
+      it 'by brand' do
+        inventories = Inventory.by_brand_id(5)
+        expect(inventories.count).to eq(0)
+      end
+      it 'by model' do
+        inventories = Inventory.by_model_id(5)
+        expect(inventories.count).to eq(0)
+      end
+      it 'by os_version' do
+        inventories = Inventory.by_os_version('Android5')
+        expect(inventories.count).to eq(0)
+      end
+      it 'by color' do
+        inventories = Inventory.by_color('greeny')
+        expect(inventories.count).to eq(0)
+      end
+    end
   end
 end
